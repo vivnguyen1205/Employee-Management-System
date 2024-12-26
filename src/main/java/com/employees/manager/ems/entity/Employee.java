c8a737fa0fec10cf8dfc42c122bb1dbcc7581882
Vivian <vivnguyen1205@gmail.com>
2024-12-26 13:27:16 -0500

    test

---
+++ src/main/java/com/employees/manager/ems/entity/Employee.java
@@ -0,0 +1,27 @@
+package com.employees.manager.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="email", nullable = false, unique = true)
+    private String email;
+}
